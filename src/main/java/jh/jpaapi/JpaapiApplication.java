4934004965a3da597aec81457c6faec5be5a59d5
jehoon6165 <83596698+jehoon6165@users.noreply.github.com>
2021-06-01 00:46:43 +0900

    study init +

--- src/main/java/jh/jpaapi/JpaapiApplication.java
+++ src/main/java/jh/jpaapi/JpaapiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JpaapiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JpaapiApplication.class, args);
 	}
-
 }
--- src/main/java/jh/jpaapi/doamin/item/Item.java
+++ src/main/java/jh/jpaapi/doamin/item/Item.java
@@ -1,6 +1,7 @@
 package jh.jpaapi.doamin.item;
 
 import jh.jpaapi.doamin.Category;
+import jh.jpaapi.doamin.exption.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -29,4 +30,22 @@ public abstract class Item {
     )
     private List<Category> categories = new ArrayList<>();
 
+    //비즈니스 로직
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
